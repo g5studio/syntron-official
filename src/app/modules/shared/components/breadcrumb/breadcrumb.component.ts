2fe8bcf9097424e68561049b890358348b31bb22
william <hr.g5studio@gmail.com>
2023-12-02 22:12:14 +0800

    feat: 調整麵包屑元件

--- src/app/modules/shared/components/breadcrumb/breadcrumb.component.ts
+++ src/app/modules/shared/components/breadcrumb/breadcrumb.component.ts
@@ -1,17 +1,23 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Breadcrumb, BreadcrumbConfig } from '@shared/constants';
+
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss']
 })
-export class BreadcrumbComponent implements OnInit {
+export class BreadcrumbComponent {
 
-  @Input() breadcrumbs: string[] = [];
-
-  constructor() { }
-
-  ngOnInit(): void {
+  get breadcrumbs(): Breadcrumb[] {
+    return BreadcrumbConfig[this.router.url] ?? [];
   }
+
+  get isShow(): boolean {
+    return this.breadcrumbs.length > 0;
+  }
+
+  constructor(public router: Router) { }
 
 }
