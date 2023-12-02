d1838f8e32043d74f94c36768fddfc0a72ee257b
WayneFu0501 <wayne@zuso.ai>
2023-12-02 22:13:12 +0800

    feat: news list render with title, description, images

--- src/app/modules/guideline/guideline-routing.module.ts
+++ src/app/modules/guideline/guideline-routing.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { OverviewPageComponent } from './pages/overview-page/overview-page.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {OverviewPageComponent} from './pages/overview-page/overview-page.component';
 import {NewsPageComponent} from "./pages/news-page/news-page.component";
 import {NewsDetailPageComponent} from "./pages/news-detail-page/news-detail-page.component";
 
 const routes: Routes = [
-  { path: 'home', component: OverviewPageComponent },
-  { path: 'news', component: NewsPageComponent },
-  { path: 'news/detail', component: NewsDetailPageComponent },
+  {path: 'home', component: OverviewPageComponent},
+  {
+    path: 'news', component: NewsPageComponent,
+    children: [
+      {path: 'detail', component: NewsDetailPageComponent}
+    ]
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class GuidelineRoutingModule { }
+export class GuidelineRoutingModule {
+}
