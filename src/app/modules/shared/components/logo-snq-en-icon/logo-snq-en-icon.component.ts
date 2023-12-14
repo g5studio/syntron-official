51a67a67491c157d47e7a490ba14ce16c1c8cd29
ga700707 <ga700557@gmail.com>
2023-12-14 10:05:58 +0800

    增加medicine icon

---
+++ src/app/modules/shared/components/logo-snq-en-icon/logo-snq-en-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-snq-en-icon',
+  templateUrl: './logo-snq-en-icon.component.svg',
+  styleUrls: ['./logo-snq-en-icon.component.scss']
+})
+export class LogoSnqEnIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
