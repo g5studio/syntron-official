d70fd806fc1e2d96e38ba6071d207f7c06e0f5a3
ga700707 <ga700557@gmail.com>
2023-12-14 22:33:30 +0800

    增加about page

--- src/app/modules/common/pages/about-us-page/about-us-page.component.ts
+++ src/app/modules/common/pages/about-us-page/about-us-page.component.ts
@@ -1,17 +1,250 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { Device } from '@shared/enums';
+import { LayoutService } from '@shared/services/layout.service';
+import { takeUntil } from 'rxjs';
 import { BasePage } from 'src/utilities/bases';
 
-
+interface ISolutionSlide {
+  title: string;
+  content: string;
+  img: {
+    pc: string;
+    pad: string;
+    mobile: string;
+  };
+}
 @Component({
   selector: 'app-about-us-page',
   templateUrl: './about-us-page.component.html',
-  styleUrls: ['./about-us-page.component.scss']
+  styleUrls: ['./about-us-page.component.scss'],
 })
-export class AboutUsPageComponent extends BasePage {
+export class AboutUsPageComponent extends BasePage implements OnInit {
+  // filterNews: News[] = [];
+  currentPage: number = 1;
+  @ViewChild('company', { static: false }) company?: ElementRef<HTMLElement>;
+  @ViewChild('solution') solution?: ElementRef<HTMLElement>;
+  @ViewChild('custom') custom?: ElementRef<HTMLElement>;
+  @ViewChild('banner') banner?: ElementRef<HTMLElement>;
+  @ViewChild('leftSide') leftSide?: ElementRef<HTMLElement>;
 
-  constructor() {
+  tabs: string[] = ['SYNTRON企業願景', '解決方案', '我們的客戶'];
+  focusTab: string = '';
+
+  constructor(private renderer: Renderer2, private $layout: LayoutService) {
     super();
   }
+  public coverImg = '';
+  public solution_slide: ISolutionSlide[] = [
+    {
+      title: '照護服務',
+      content: '長照照護系統、照護流程輔導諮詢、照護人員培訓',
+      img: {
+        pc: 'assets/product_pc/banner_news_caring_pc.png',
+        pad: 'assets/product_pc/banner_news_caring_pad.png',
+        mobile: 'assets/product_pc/banner_news_caring_mobile.png',
+      },
+    },
+    {
+      title: '藥品管理',
+      content: '智慧藥櫃系統、藥品管理系統、重要資源管理',
+      img: {
+        pc: 'assets/product_pc/banner_news_medication_pc.png',
+        pad: 'assets/product_pc/banner_news_medication_pad.png',
+        mobile: 'assets/product_pc/banner_news_medication_mobile.png',
+      },
+    },
+    {
+      title: '照護服務',
+      content: '長照照護系統、照護流程輔導諮詢、照護人員培訓',
+      img: {
+        pc: 'assets/product_pc/banner_news_caring_pc.png',
+        pad: 'assets/product_pc/banner_news_caring_pad.png',
+        mobile: 'assets/product_pc/banner_news_caring_mobile.png',
+      },
+    },
+    {
+      title: '藥品管理',
+      content: '智慧藥櫃系統、藥品管理系統、重要資源管理',
+      img: {
+        pc: 'assets/product_pc/banner_news_medication_pc.png',
+        pad: 'assets/product_pc/banner_news_medication_pad.png',
+        mobile: 'assets/product_pc/banner_news_medication_mobile.png',
+      },
+    },
+  ];
 
+  public logo_list: string[] = [
+    'logo_ntuh',
+    'logo_vetern',
+    'logo_taipei_city_hosptial',
+    'logo_chiayi_health',
+    'logo_marlabs',
+    'logo_culture',
+    'logo_laber',
+    'logo_new_taipei_gov',
+    'logo_cjcu',
+    'logo_chengkung_university',
+    'logo_kinmen',
+    'logo_huafan',
+    'logo_carrefour',
+    'logo_esun_bank',
+    'logo_taiwan_oil',
+    'logo_twpower',
+  ];
 
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  // @HostListener('window:scroll')
+  // onWindowScroll() {
+  //   const el = this.leftSide!.nativeElement;
+  //   const distance = el.getBoundingClientRect().top;
+  //   console.log(window.pageYOffset);
+  //   if (window.pageYOffset > distance) {
+  //     el.style.position = 'fixed';
+  //     el.style.top = '10rem';
+  //     // 设置其他样式
+  //   } else {
+  //     el.style.position = 'absolute';
+  //     el.style.top = '5rem';
+  //     // 移除其他样式
+  //   }
+  // }
+  protected override onInit(): void {
+    // this.$news.fetchNews$()
+    //   .subscribe(() => {
+    //     this.filterNews = this.$news.news;
+    //     this.tabs = this.tabs.map((tab) => {
+    //       let length = tab === '全部最新消息' ? this.filterNews.length : this.filterNews.filter((news) => news.category === tab).length;
+    //       return `${tab} (${length})`;
+    //     });
+    //     this.focusTab = this.tabs[0];
+    //   });
+    this.$layout.$mainScroll
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((scroll) => {
+        const scrollDistance = (scroll.target as HTMLElement).scrollTop;
+        const distanceFromTop =
+          this.banner!.nativeElement.offsetTop +
+          this.banner!.nativeElement.offsetHeight -
+          90;
+        const leftSide = this.leftSide!.nativeElement;
+        if (scrollDistance > distanceFromTop) {
+          leftSide.style.position = 'fixed';
+          leftSide.style.top = '9rem';
+        } else {
+          leftSide.style.position = 'absolute';
+          leftSide.style.top = '';
+        }
+      });
+    this.setCoverImg();
+  }
+
+  changePage($event: number) {
+    // this.currentPage = $event;
+    // this.$window.scrollTo(0);
+  }
+
+  changeTab($event: string) {
+    // if ($event.includes('SYNTRON企業願景')) {
+    //   this.company!.nativeElement.scrollIntoView({
+    //     behavior: 'smooth',
+    //     block: 'start',
+    //     inline: 'nearest',
+    //   });
+    // } else if ($event.includes('解決方案')) {
+    //   this.solution!.nativeElement.scrollIntoView({
+    //     behavior: 'smooth',
+    //     block: 'start',
+    //     inline: 'nearest',
+    //   });
+    // } else if ($event.includes('我們的客戶')) {
+    //   this.custom!.nativeElement.scrollIntoView({
+    //     behavior: 'smooth',
+    //     block: 'start',
+    //     inline: 'nearest',
+    //   });
+    // } else {
+    //   this.company!.nativeElement.scrollIntoView({
+    //     behavior: 'smooth',
+    //     block: 'center',
+    //   });
+    // }
+    // this.$window.scrollTo(0);
+    // window.scrollTo({
+    //     top: 100,
+    //     behavior: 'smooth',
+    //   });
+    const distanceFromTop =
+    this.banner!.nativeElement.offsetTop +
+    this.banner!.nativeElement.offsetHeight -
+    150;
+    console.log(distanceFromTop)
+    if ($event.includes('SYNTRON企業願景')) {
+      console.log(this.company!.nativeElement.offsetTop)
+      this.$window.scrollTo(this.company!.nativeElement.offsetTop + distanceFromTop);
+
+    } else if ($event.includes('解決方案')) {
+      this.$window.scrollTo(this.solution!.nativeElement.offsetTop + distanceFromTop);
+
+    } else if ($event.includes('我們的客戶')) {
+      this.$window.scrollTo(this.custom!.nativeElement.offsetTop + distanceFromTop);
+    } 
+ 
+    // setTimeout(() => {
+    //   this.$window.scrollTo(this.solution!.nativeElement.offsetTop);
+    // }, 1000);
+    // setTimeout(() => {
+    //   window.scrollBy(0, 1000);
+    // }, 500);
+  }
+
+  toDetailPage(id: number) {
+    // const focusTab = this.focusTab.split(' ')[0];
+    // let focusTabPath: string = '';
+    // switch (focusTab) {
+    //   case '全部最新消息':
+    //     focusTabPath = 'all';
+    //     break;
+    //   case '新聞媒體':
+    //     focusTabPath = 'media';
+    //     break;
+    //   case '重要通知':
+    //     focusTabPath = 'notice';
+    //     break;
+    //   case '活動通知':
+    //     focusTabPath = 'events';
+    //     break;
+    //   case '新產品':
+    //     focusTabPath = 'new-products';
+    //     break;
+    // }
+    // this.router.navigate([`/news-center/news/${focusTabPath}/detail/`], { queryParams: { id } }).then();
+  }
+  private setCoverImg(): void {
+    this.$window.device$
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((device) => {
+        switch (device) {
+          case Device.Desktop:
+            this.coverImg = 'assets/img/iStock/iStock-1328611793.jpg';
+            break;
+          case Device.Tablet:
+            this.coverImg = 'assets/img/iStock/iStock-1328611793.jpg';
+            break;
+          case Device.Mobile:
+            this.coverImg = 'assets/img/iStock/iStock-1328611793.jpg';
+            break;
+        }
+      });
+  }
 }
